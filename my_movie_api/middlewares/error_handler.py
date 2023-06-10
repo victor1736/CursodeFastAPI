7f99a59f17a8460e8f4fd830568b298cb2d24164
victor1736 <victoralfonsofernandezhoyos@gmail.com>
2023-06-10 15:01:48 -0500

    terminado de api
    aplicacion de mi api my_movie_api

---
+++ my_movie_api/middlewares/error_handler.py
@@ -0,0 +1,16 @@
+import typing
+from starlette.middleware.base import BaseHTTPMiddleware, DispatchFunction, RequestResponseEndpoint
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+from starlette.types import ASGIApp
+from fastapi import FastAPI, Request, Response
+
+class ErrorHandler(BaseHTTPMiddleware):
+    def __init__(self, app: FastAPI) -> None:
+        super().__init__(app)
+        
+    async def dispatch(self, request: Request, call_next) -> Response | JSONResponse:
+        try:
+            return await call_next(request)
+        except Exception as e:
+            return JSONResponse(status_code=500, content={'error':str(e)})
